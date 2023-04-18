7d4b7d8afa00fac3d820f76e6d61dbc5c5671839
anbehindY <yanminthwin@gmail.com>
2023-04-18 16:15:08 +0630

    Fix issues with webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -27,36 +27,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.jsx?$/,
-        exclude: ['node_modules'],
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.gif$/,
-        type: 'asset/inline',
-      },
-      {
-        test: /\.(ttf|eot|svg)$/,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      config$: './configs/app-config.js',
-      react: './vendor/react-master',
-    },
-    extensions: ['.js', '.jsx'],
-    modules: [
-      'node_modules',
-      'bower_components',
-      'shared',
-      '/shared/vendor/modules',
     ],
   },
 };

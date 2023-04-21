eff2ff1c59d9489a197c20b2ceba7f48446e77bb
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-04-21 16:50:53 +0300

    added comment pop-up

--- src/index.js
+++ src/index.js
@@ -2,23 +2,47 @@ import './styles.css';
 import Logo from './logo.png';
 import getShows from './modules/tvMazeApi.js';
 import getLikes from './modules/likes.js';
-
 // Add Logo to the header
 const divLogo = document.querySelector('.logo');
 const myLogo = new Image();
 myLogo.src = Logo;
 myLogo.classList.add('myLogo');
 divLogo.append(myLogo);
-
 let showsArray = [];
 let likesArray = [];
+const popup = (array) => {
+  const commentBtns = document.querySelectorAll('.commentBtn');
+  const commentPopup = document.querySelector('.comment-popup');
+  const closeBtn = document.querySelector('.close-popup-btn');
+  const comments = document.querySelector('.comments');
+  commentBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      const movieSummary = document.querySelector('.movie-summary');
+      const movieTitle = document.querySelector('.movie-title');
+      const popupPhoto = document.querySelector('.popup-photo');
+      const submitCommentBtn = document.querySelector('.submit-comment-btn');
+      const genre = document.querySelector('.genre');
+      submitCommentBtn.setAttribute('id', index);
+      commentPopup.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+      popupPhoto.src = array[index].image.original;
+      movieTitle.textContent = array[index].name;
+      genre.textContent = array[index].genres.join(', ');
+      movieSummary.innerHTML = array[index].summary;
+    });
+  });
+  closeBtn.addEventListener('click', () => {
+    commentPopup.style.display = 'none';
+    document.body.style.overflow = 'auto';
+    comments.textContent = 'Comments';
+  });
+};
 const cardsContainer = document.querySelector('.card-container');
 const loadShows = async () => {
   const showData = await getShows();
   const likeData = await getLikes();
   showsArray = showData;
   likesArray = likeData;
-
   showsArray.forEach((showInfo) => {
     let movieLikes = 0;
     if (likesArray.find((like) => like.item_id === showInfo.id)) {
@@ -36,5 +60,6 @@ const loadShows = async () => {
       </div>
   </div>`;
   });
+  popup(showsArray);
 };
 loadShows();

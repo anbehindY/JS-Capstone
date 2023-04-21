b137e2bc825a80436243dfd6dbb0a2eb24d5bdd5
anbehindY <yanminthwin@gmail.com>
2023-04-22 03:49:35 +0630

    Change the number of shows fetched

--- src/modules/tvMazeApi.js
+++ src/modules/tvMazeApi.js
@@ -2,7 +2,7 @@ const getShows = async () => {
   try {
     const response = await fetch('https://api.tvmaze.com/shows');
     let data = await response.json();
-    data = data.slice(0, 21);
+    data = data.slice(0, 20);
     return data;
   } catch (error) {
     throw new Error(error);

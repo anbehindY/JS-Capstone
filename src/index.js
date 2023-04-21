eaa7d10a01ad4a98360aaee67cc79c000058b886
anbehindY <yanminthwin@gmail.com>
2023-04-21 15:10:11 +0630

    Render likes data from api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import Logo from './logo.png';
-import loadShows from './modules/loadShows.js';
+import getShows from './modules/tvMazeApi.js';
+import getLikes from './modules/likes.js';
 
 // Add Logo to the header
 const divLogo = document.querySelector('.logo');
@@ -9,4 +10,31 @@ myLogo.src = Logo;
 myLogo.classList.add('myLogo');
 divLogo.append(myLogo);
 
+let showsArray = [];
+let likesArray = [];
+const cardsContainer = document.querySelector('.card-container');
+const loadShows = async () => {
+  const showData = await getShows();
+  const likeData = await getLikes();
+  showsArray = showData;
+  likesArray = likeData;
+
+  showsArray.forEach((showInfo) => {
+    let movieLikes = 0;
+    if (likesArray.find((like) => like.item_id === showInfo.id)) {
+      movieLikes = likesArray.find((like) => like.item_id === showInfo.id).likes;
+    }
+    cardsContainer.innerHTML += `<div class="card">
+      <img class="cardImg" src="${showInfo.image.original}" alt="${showInfo.name}">
+      <div class="caption">
+          <p class="name">${showInfo.name}</p>
+          <p class="likes" likes-id="${showInfo.id}">${movieLikes} ${movieLikes >= 1 ? 'likes' : 'like'}</p>
+      </div>
+      <div class="btn">
+      <button id="${showInfo.id}" class="commentBtn" type="button">Comment</button>
+      <button id="${showInfo.id}" class="likeBtn" show-id="${showInfo.id}" type="button">Like</button>
+      </div>
+  </div>`;
+  });
+};
 loadShows();
